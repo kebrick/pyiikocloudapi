6f892cf2dbeeacddcefa55038ce2a662c0436ff0
kebrick <ruban.kebr@gmail.com>
2022-12-21 15:01:53 +0300

    Edit: v0.0.4

--- pyiikocloudapi/__init__.py
+++ pyiikocloudapi/__init__.py
@@ -4,6 +4,6 @@ from .models import *
 # import card
 NAME = "pyiikocloudapi"
 __author__ = 'kebrick'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
 __email__ = 'ruban.kebr@gmail.com'
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-VERSION = '0.0.3'
+VERSION = '0.0.4'
 setup(
     name='pyiikocloudapi',
     version=VERSION,
